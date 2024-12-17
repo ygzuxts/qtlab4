c508036fb757488cf2121e4e8518fa3c14322183
yzx <1097823348@qq.com>
2024-12-17 15:48:59 +0800

    ui设计完成

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_startStopButton_clicked()
+{
+    ui->startStopButton->setText("启动服务器");
+    static bool started = false;
+    started = !started;
+    if (started) {
+        ui->startStopButton->setText("停止服务器");
+        logMessage("服务器已经启动");
+    } else {
+        ui->startStopButton->setText("启动服务器");
+        logMessage("服务器已经停止");
+    }
+
+}
+
+void MainWindow::logMessage(const QString &msg)
+{
+    ui->logEditor->appendPlainText(msg);
+}
+
